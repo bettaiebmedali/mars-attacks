1aebc288ab70f93966bfa93b88baa3e932063f42
M.BETTAIEB <bettaiebmedaly@gmail.com>
2026-07-28 14:33:10 +0000

    workflow validate

--- tech-scout/backend/app/api/badge_requests.py
+++ tech-scout/backend/app/api/badge_requests.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from app.models.user_badge import UserBadge
+from app.security.dependencies import get_current_user
+from app.security.dependencies import require_role
 from app.db.database import get_db
 
 from app.models.badge_request import BadgeRequest
@@ -18,6 +20,154 @@ router = APIRouter(
     tags=["Badge Requests"]
 )
 
+@router.get("/pending")
+def get_pending_requests(
+    db: Session = Depends(get_db),
+    current_user = Depends(
+        require_role("MENTOR", "ADMIN")
+    )
+):
+
+    requests = (
+        db.query(BadgeRequest)
+        .filter(
+            BadgeRequest.status == "PENDING"
+        )
+        .all()
+    )
+
+    result = []
+
+    for request in requests:
+        result.append(
+            {
+                "id": request.id,
+                "user": request.user.email,
+                "badge": request.badge.name,
+                "status": request.status,
+                "comment": request.comment,
+                "requested_at": request.requested_at
+            }
+        )
+
+    return result
+
+@router.post("/{request_id}/approve")
+def approve_request(
+
+    request_id: int,
+
+    db: Session = Depends(get_db),
+
+    current_user = Depends(
+        require_role("MENTOR", "ADMIN")
+    )
+):
+
+    badge_request = (
+        db.query(BadgeRequest)
+        .filter(
+            BadgeRequest.id == request_id
+        )
+        .first()
+    )
+
+
+    if not badge_request:
+        raise HTTPException(
+            404,
+            "Request not found"
+        )
+
+
+    if badge_request.status != "PENDING":
+        raise HTTPException(
+            400,
+            "Request already processed"
+        )
+
+
+    user_badge = UserBadge(
+
+        user_id=badge_request.user_id,
+
+        badge_id=badge_request.badge_id,
+
+        assigned_by=current_user.id,
+
+        comment="Approved by mentor"
+    )
+
+
+    db.add(user_badge)
+
+
+    badge_request.status = "APPROVED"
+
+    badge_request.validated_by = current_user.id
+
+
+    db.commit()
+
+
+    return {
+
+        "message":"Badge approved",
+
+        "user":badge_request.user.email,
+
+        "badge":badge_request.badge.name
+
+    }
+
+@router.post("/{request_id}/reject")
+def reject_request(
+
+    request_id: int,
+
+    db: Session = Depends(get_db),
+
+    current_user = Depends(
+        require_role("MENTOR", "ADMIN")
+    )
+):
+
+    badge_request = (
+        db.query(BadgeRequest)
+        .filter(
+            BadgeRequest.id == request_id
+        )
+        .first()
+    )
+
+
+    if not badge_request:
+        raise HTTPException(
+            404,
+            "Request not found"
+        )
+
+
+    if badge_request.status != "PENDING":
+        raise HTTPException(
+            400,
+            "Request already processed"
+        )
+
+
+    badge_request.status = "REJECTED"
+
+    badge_request.validated_by = current_user.id
+
+
+    db.commit()
+
+
+    return {
+
+        "message":"Badge request rejected"
+
+    }
 
 @router.post(
     "/",
--- tech-scout/backend/app/models/badge_request.py
+++ tech-scout/backend/app/models/badge_request.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
